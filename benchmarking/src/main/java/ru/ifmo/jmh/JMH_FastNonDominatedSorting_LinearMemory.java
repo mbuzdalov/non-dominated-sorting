3a1eedf1a60e373ad03a58ab5b32f4d9cab8333a
Maxim Buzdalov <mbuzdalov@gmail.com>
2017-07-27 03:00:44 +0300

    Revert to old-style benchmarking configuration

--- benchmarking/src/main/java/ru/ifmo/jmh/JMH_FastNonDominatedSorting_LinearMemory.java
+++ benchmarking/src/main/java/ru/ifmo/jmh/JMH_FastNonDominatedSorting_LinearMemory.java
@@ -1,22 +1,12 @@
 package ru.ifmo.jmh;
 
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
-
 import ru.ifmo.FastNonDominatedSorting;
 import ru.ifmo.NonDominatedSortingFactory;
 
+@SuppressWarnings("unused")
 public class JMH_FastNonDominatedSorting_LinearMemory extends AbstractBenchmark {
     @Override
     protected NonDominatedSortingFactory getFactory() {
         return FastNonDominatedSorting.getLinearMemoryImplementation();
-    }
-
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(JMH_FastNonDominatedSorting_LinearMemory.class.getSimpleName()).build();
-        new Runner(opt).run();
     }
 }
--- benchmarking/src/main/java/ru/ifmo/jmh/JMH_JensenFortinBuzdalov_Fenwick.java
+++ benchmarking/src/main/java/ru/ifmo/jmh/JMH_JensenFortinBuzdalov_Fenwick.java
@@ -1,21 +1,12 @@
 package ru.ifmo.jmh;
 
-import org.openjdk.jmh.runner.Runner;
-import org.openjdk.jmh.runner.RunnerException;
-import org.openjdk.jmh.runner.options.Options;
-import org.openjdk.jmh.runner.options.OptionsBuilder;
 import ru.ifmo.JensenFortinBuzdalov;
 import ru.ifmo.NonDominatedSortingFactory;
 
+@SuppressWarnings("unused")
 public class JMH_JensenFortinBuzdalov_Fenwick extends AbstractBenchmark {
     @Override
     protected NonDominatedSortingFactory getFactory() {
         return JensenFortinBuzdalov.getFenwickSweepImplementation();
-    }
-
-    public static void main(String[] args) throws RunnerException {
-        Options opt = new OptionsBuilder()
-                .include(JMH_JensenFortinBuzdalov_Fenwick.class.getSimpleName()).build();
-        new Runner(opt).run();
     }
 }
