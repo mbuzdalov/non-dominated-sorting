8b17571c7d6796c67587d7a7231de269238a0880
Maxim Buzdalov <mbuzdalov@gmail.com>
2017-07-27 03:26:31 +0300

    Get rid of 'instance' field, as forks already do that

--- benchmarking/src/main/java/ru/ifmo/jmh/AbstractBenchmark.java
+++ benchmarking/src/main/java/ru/ifmo/jmh/AbstractBenchmark.java
@@ -1,24 +1,20 @@
 package ru.ifmo.jmh;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
 import org.openjdk.jmh.annotations.*;
 
 import ru.ifmo.NonDominatedSorting;
 import ru.ifmo.NonDominatedSortingFactory;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
 
 @State(Scope.Thread)
 @Fork(5)
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 public abstract class AbstractBenchmark {
-    @Param({"0", "1", "2", "3", "4"})
-    @SuppressWarnings({"unused"})
-    private int instance;
-
-    @Param({"2", "3", "4", "6", "8", "10", "15", "20"})
+    @Param({"2", "3", "4", "5", "10"})
     private int dimension;
 
     private NonDominatedSorting sorting;
@@ -40,28 +36,28 @@ public abstract class AbstractBenchmark {
     }
 
     @Benchmark
-    @Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 20, time = 1)
     @Measurement(iterations = 5, time = 1)
     public int cloudBenchmark_N10() {
         return datasets.get(0).runSortingOnMe(sorting);
     }
 
     @Benchmark
-    @Warmup(iterations = 5, time = 100, timeUnit = TimeUnit.MILLISECONDS)
+    @Warmup(iterations = 20, time = 1)
     @Measurement(iterations = 5, time = 1)
     public int cloudBenchmark_N100() {
         return datasets.get(1).runSortingOnMe(sorting);
     }
 
     @Benchmark
-    @Warmup(iterations = 5, time = 1)
+    @Warmup(iterations = 20, time = 1)
     @Measurement(iterations = 5, time = 1)
     public int cloudBenchmark_N1000() {
         return datasets.get(2).runSortingOnMe(sorting);
     }
 
     @Benchmark
-    @Warmup(iterations = 5, time = 10)
+    @Warmup(iterations = 20, time = 10)
     @Measurement(iterations = 5, time = 10)
     public int cloudBenchmark_N10000() {
         return datasets.get(3).runSortingOnMe(sorting);
