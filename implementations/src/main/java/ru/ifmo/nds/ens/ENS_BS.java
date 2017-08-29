5bdfd36e6427e8ea415c2afe9b68fc3d316ddd13
Maxim Buzdalov <mbuzdalov@gmail.com>
2017-08-29 17:51:59 +0300

    Make inline-oriented improvements to ENS implementations.

--- implementations/src/main/java/ru/ifmo/nds/ens/ENS_BS.java
+++ implementations/src/main/java/ru/ifmo/nds/ens/ENS_BS.java
@@ -5,23 +5,28 @@ public class ENS_BS extends ENSBase {
         super(maximumPoints, maximumDimension);
     }
 
+    private int findRank(double[][] points, int index, int maxRank) {
+        double[] point = points[index];
+        int leftRank = -1, rightRank = maxRank + 1;
+        while (rightRank - leftRank > 1) {
+            int currRank = (leftRank + rightRank) >>> 1;
+            if (frontDominates(currRank, points, point)) {
+                leftRank = currRank;
+            } else {
+                rightRank = currRank;
+            }
+        }
+        return rightRank;
+    }
+
     @Override
     void sortCheckedImpl(double[][] points, int[] ranks, int maximalMeaningfulRank) {
         int n = ranks.length;
         int maxRank = -1;
         for (int i = 0; i < n; ++i) {
             int index = indices[i];
-            double[] point = points[index];
-            int leftRank = -1, rightRank = maxRank + 1;
-            while (rightRank - leftRank > 1) {
-                int currRank = (leftRank + rightRank) >>> 1;
-                if (frontDominates(currRank, points, point)) {
-                    leftRank = currRank;
-                } else {
-                    rightRank = currRank;
-                }
-            }
-            maxRank = setRank(index, ranks, rightRank, maxRank);
+            int rank = findRank(points, index, maxRank);
+            maxRank = setRank(index, ranks, rank, maxRank);
         }
     }
 
--- implementations/src/main/java/ru/ifmo/nds/ens/ENS_SS.java
+++ implementations/src/main/java/ru/ifmo/nds/ens/ENS_SS.java
@@ -5,21 +5,26 @@ public class ENS_SS extends ENSBase {
         super(maximumPoints, maximumDimension);
     }
 
+    private int findRank(double[][] points, int index, int maxRank) {
+        double[] point = points[index];
+        int currRank = 0;
+        while (currRank <= maxRank) {
+            if (frontDominates(currRank, points, point)) {
+                ++currRank;
+            } else {
+                break;
+            }
+        }
+        return currRank;
+    }
+
     @Override
     void sortCheckedImpl(double[][] points, int[] ranks, int maximalMeaningfulRank) {
         int n = ranks.length;
         int maxRank = -1;
         for (int i = 0; i < n; ++i) {
             int index = indices[i];
-            double[] point = points[index];
-            int currRank = 0;
-            while (currRank <= maxRank) {
-                if (frontDominates(currRank, points, point)) {
-                    ++currRank;
-                } else {
-                    break;
-                }
-            }
+            int currRank = findRank(points, index, maxRank);
             maxRank = setRank(index, ranks, currRank, maxRank);
         }
     }
