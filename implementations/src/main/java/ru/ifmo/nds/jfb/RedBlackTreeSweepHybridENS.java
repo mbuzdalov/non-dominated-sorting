dd63f90b1e3b186d041f87cef609c0fbf1fbf290
Maxim Buzdalov <mbuzdalov@gmail.com>
2017-09-27 16:03:01 +0300

    Move ENS state to a thread local.

--- implementations/src/main/java/ru/ifmo/nds/jfb/RedBlackTreeSweepHybridENS.java
+++ implementations/src/main/java/ru/ifmo/nds/jfb/RedBlackTreeSweepHybridENS.java
@@ -3,96 +3,92 @@ package ru.ifmo.nds.jfb;
 public class RedBlackTreeSweepHybridENS extends RedBlackTreeSweep {
     private static final int MAX_SIZE = 400;
 
-    private int[][] ensIndices;
-    private int[] ensSize;
-    private int[] ensRank;
-    private int[] ensNext;
-    private int ensFirst, ensCount;
-    private int lastCurr, lastIndex;
+    private ThreadLocal<ENSHelper> ensHelper = ThreadLocal.withInitial(ENSHelper::new);
 
     public RedBlackTreeSweepHybridENS(int maximumPoints, int maximumDimension) {
         super(maximumPoints, maximumDimension);
-
-        ensIndices = new int[MAX_SIZE][MAX_SIZE];
-        ensNext = new int[MAX_SIZE];
-        ensRank = new int[MAX_SIZE];
-        ensSize = new int[MAX_SIZE];
     }
 
-    private void ensInit() {
-        ensFirst = -1;
-        ensCount = 0;
-    }
+    private final class ENSHelper {
+        private int[][] ensIndices = new int[MAX_SIZE][MAX_SIZE];
+        private int[] ensSize = new int[MAX_SIZE];
+        private int[] ensRank = new int[MAX_SIZE];
+        private int[] ensNext = new int[MAX_SIZE];
+        private int ensFirst, ensCount;
+        private int lastCurr, lastIndex;
 
-    private int ensFindRank(int index, int maxObj) {
-        int curr = ensFirst;
-        int dominatingIndex = -1;
-        int pointRank = ranks[index];
-        lastCurr = -1;
-        lastIndex = index;
-        while (curr != -1 && ensRank[curr] >= pointRank) {
-            boolean dominates = false;
-            int[] ensCurrIndices = ensIndices[curr];
-            for (int i = ensSize[curr] - 1; i >= 0; --i) {
-                if (strictlyDominatesAssumingNotSame(ensCurrIndices[i], index, maxObj)) {
-                    dominates = true;
+        void init() {
+            ensFirst = -1;
+            ensCount = 0;
+        }
+
+        int findRank(int index, int maxObj) {
+            int curr = ensFirst;
+            int dominatingIndex = -1;
+            int pointRank = ranks[index];
+            lastCurr = -1;
+            lastIndex = index;
+            while (curr != -1 && ensRank[curr] >= pointRank) {
+                boolean dominates = false;
+                int[] ensCurrIndices = ensIndices[curr];
+                for (int i = ensSize[curr] - 1; i >= 0; --i) {
+                    if (strictlyDominatesAssumingNotSame(ensCurrIndices[i], index, maxObj)) {
+                        dominates = true;
+                        break;
+                    }
+                }
+                if (dominates) {
+                    dominatingIndex = curr;
                     break;
                 }
+                lastCurr = curr;
+                curr = ensNext[curr];
             }
-            if (dominates) {
-                dominatingIndex = curr;
-                break;
-            }
-            lastCurr = curr;
-            curr = ensNext[curr];
+            return dominatingIndex == -1 ? 0 : ensRank[dominatingIndex] + 1;
         }
-        return dominatingIndex == -1 ? 0 : ensRank[dominatingIndex] + 1;
-    }
 
-    private int ensCreateNewRow(int index, int pointRank, int next) {
-        ensRank[ensCount] = pointRank;
-        ensNext[ensCount] = next;
-        ensSize[ensCount] = 1;
-        ensIndices[ensCount][0] = index;
-        return ensCount++;
-    }
+        int createNewRow(int index, int pointRank, int next) {
+            ensRank[ensCount] = pointRank;
+            ensNext[ensCount] = next;
+            ensSize[ensCount] = 1;
+            ensIndices[ensCount][0] = index;
+            return ensCount++;
+        }
 
-    private void ensInsertPoint(int index) {
-        int pointRank = ranks[index];
-        if (ensFirst == -1 || ensRank[ensFirst] < pointRank) {
-            ensFirst = ensCreateNewRow(index, pointRank, ensFirst);
-        } else if (pointRank == ensRank[ensFirst]) {
-            ensIndices[ensFirst][ensSize[ensFirst]++] = index;
-        } else {
-            int prev = index == lastIndex && lastCurr >= 0 ? lastCurr : ensFirst;
-            if (ensRank[prev] == pointRank) {
-                ensIndices[prev][ensSize[prev]++] = index;
+        void insertPoint(int index) {
+            int pointRank = ranks[index];
+            if (ensFirst == -1 || ensRank[ensFirst] < pointRank) {
+                ensFirst = createNewRow(index, pointRank, ensFirst);
+            } else if (pointRank == ensRank[ensFirst]) {
+                ensIndices[ensFirst][ensSize[ensFirst]++] = index;
             } else {
-                int next = ensNext[prev];
-                while (next != -1 && ensRank[next] > pointRank) {
-                    if (ensRank[next] >= ensRank[prev]) {
-                        throw new AssertionError();
-                    }
-                    prev = next;
-                    next = ensNext[next];
-                }
-                if (next != -1 && ensRank[next] == pointRank) {
-                    ensIndices[next][ensSize[next]++] = index;
+                int prev = index == lastIndex && lastCurr >= 0 ? lastCurr : ensFirst;
+                if (ensRank[prev] == pointRank) {
+                    ensIndices[prev][ensSize[prev]++] = index;
                 } else {
-                    ensNext[prev] = ensCreateNewRow(index, pointRank, next);
+                    int next = ensNext[prev];
+                    while (next != -1 && ensRank[next] > pointRank) {
+                        if (ensRank[next] >= ensRank[prev]) {
+                            throw new AssertionError();
+                        }
+                        prev = next;
+                        next = ensNext[next];
+                    }
+                    if (next != -1 && ensRank[next] == pointRank) {
+                        ensIndices[next][ensSize[next]++] = index;
+                    } else {
+                        ensNext[prev] = createNewRow(index, pointRank, next);
+                    }
                 }
             }
         }
     }
+
 
     @Override
     protected void closeImpl() throws Exception {
         super.closeImpl();
-
-        ensIndices = null;
-        ensNext = null;
-        ensRank = null;
-        ensSize = null;
+        ensHelper = null;
     }
 
     @Override
@@ -111,14 +107,15 @@ public class RedBlackTreeSweepHybridENS extends RedBlackTreeSweep {
 
     @Override
     protected int helperAHook(int from, int until, int obj) {
-        ensInit();
+        ENSHelper helper = ensHelper.get();
+        helper.init();
         int minOverflow = until;
         for (int i = from; i < until; ++i) {
             int ii = indices[i];
-            int r = Math.max(ranks[ii], ensFindRank(ii, obj));
+            int r = Math.max(ranks[ii], helper.findRank(ii, obj));
             ranks[ii] = r;
             if (r <= maximalMeaningfulRank) {
-                ensInsertPoint(ii);
+                helper.insertPoint(ii);
             } else if (minOverflow > i) {
                 minOverflow = i;
             }
@@ -133,14 +130,15 @@ public class RedBlackTreeSweepHybridENS extends RedBlackTreeSweep {
 
     @Override
     protected int helperBHook(int goodFrom, int goodUntil, int weakFrom, int weakUntil, int obj) {
-        ensInit();
+        ENSHelper helper = ensHelper.get();
+        helper.init();
         int minOverflowed = weakUntil;
         for (int gi = goodFrom, wi = weakFrom; wi < weakUntil; ++wi) {
             while (gi < goodUntil && indices[gi] < indices[wi]) {
-                ensInsertPoint(indices[gi++]);
+                helper.insertPoint(indices[gi++]);
             }
             int ii = indices[wi];
-            int r = Math.max(ranks[ii], ensFindRank(ii, obj));
+            int r = Math.max(ranks[ii], helper.findRank(ii, obj));
             ranks[ii] = r;
             if (minOverflowed > wi && r > maximalMeaningfulRank) {
                 minOverflowed = wi;
