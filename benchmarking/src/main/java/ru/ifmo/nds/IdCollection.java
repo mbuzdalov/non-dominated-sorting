3f729dcc00c169b4fa1ba840785788cfde769d9c
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-01-06 15:34:42 +0300

    Add uniform.correlated dataset. It is a worst-case for ENS-NDT.

--- benchmarking/src/main/java/ru/ifmo/nds/IdCollection.java
+++ benchmarking/src/main/java/ru/ifmo/nds/IdCollection.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import ru.ifmo.nds.rundb.Dataset;
 import ru.ifmo.nds.rundb.generators.DatasetGenerator;
+import ru.ifmo.nds.rundb.generators.UniformCorrelated;
 import ru.ifmo.nds.rundb.generators.UniformHypercube;
 import ru.ifmo.nds.rundb.generators.UniformHyperplanes;
 
@@ -48,6 +49,7 @@ public final class IdCollection {
     static {
         addGenerator(UniformHypercube.getInstance());
         addGenerator(UniformHyperplanes.getInstance());
+        addGenerator(UniformCorrelated.getInstance());
     }
 
     private static void addNonDominatedSortingFactory(String id, NonDominatedSortingFactory factory) {
