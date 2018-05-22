8ef672b0f29c6b27e9c7954c469d68e78d49821c
Maxim Buzdalov <mbuzdalov@gmail.com>
2018-05-22 17:35:05 +0300

    Isolate a single-element case on the range handle level.
    This explicitly makes a (mostly) one-rank dataset faster in a vEB impl.

--- implementations/src/main/java/ru/ifmo/nds/util/VanEmdeBoasRankQueryStructure.java
+++ implementations/src/main/java/ru/ifmo/nds/util/VanEmdeBoasRankQueryStructure.java
@@ -3,16 +3,59 @@ package ru.ifmo.nds.util;
 import ru.ifmo.nds.util.veb.VanEmdeBoasSet;
 
 public class VanEmdeBoasRankQueryStructure extends RankQueryStructure {
-    private final VanEmdeBoasRangeHandle theOnlyHandle;
+    private final EmptyOrSingleHandle smallHandle;
 
     public VanEmdeBoasRankQueryStructure(int maximumPoints) {
-        theOnlyHandle = new VanEmdeBoasRangeHandle(maximumPoints);
+        VanEmdeBoasRangeHandle bigHandle = new VanEmdeBoasRangeHandle(maximumPoints);
+        smallHandle = new EmptyOrSingleHandle(bigHandle);
     }
 
     @Override
     public RangeHandle createHandle(int storageStart, int from, int until, int[] indices, int[] keys) {
-        theOnlyHandle.clear();
-        return theOnlyHandle;
+        smallHandle.clear();
+        return smallHandle;
+    }
+
+    private static class EmptyOrSingleHandle extends RangeHandle {
+        private final VanEmdeBoasRangeHandle forTwoOrMore;
+        private int key = Integer.MAX_VALUE, value = Integer.MIN_VALUE;
+
+        private EmptyOrSingleHandle(VanEmdeBoasRangeHandle forTwoOrMore) {
+            this.forTwoOrMore = forTwoOrMore;
+        }
+
+        @Override
+        public RangeHandle put(int key, int value) {
+            if (key < this.key) {
+                if (value < this.value) {
+                    forTwoOrMore.put(this.key, this.value);
+                    return forTwoOrMore.put(key, value);
+                }
+                this.key = key;
+                this.value = value;
+            } else if (key == this.key) {
+                if (this.value < value) {
+                    this.value = value;
+                }
+            } else {
+                if (value > this.value) {
+                    forTwoOrMore.put(this.key, this.value);
+                    return forTwoOrMore.put(key, value);
+                }
+            }
+            return this;
+        }
+
+        @Override
+        public int getMaximumWithKeyAtMost(int key, int minimumMeaningfulAnswer) {
+            return key >= this.key ? value : -1;
+        }
+
+        void clear() {
+            forTwoOrMore.clear();
+            this.key = Integer.MAX_VALUE;
+            this.value = Integer.MIN_VALUE;
+        }
     }
 
     private static class VanEmdeBoasRangeHandle extends RangeHandle {
