f70716f43b3549a5e27f8a501e0d497255b3a853
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-01-07 22:18:52 +0300

    Refactor DCNS to have a hopefully simpler and faster logic.

--- implementations/src/main/java/ru/ifmo/nds/dcns/DCNSBase.java
+++ implementations/src/main/java/ru/ifmo/nds/dcns/DCNSBase.java
@@ -52,59 +52,93 @@ public abstract class DCNSBase extends NonDominatedSorting {
 
     abstract int findRank(int targetFrom, int targetUntil, int pointIndex);
 
+    private int writeOutAndFindRanksWithoutParentChecking(int insertedFrontStart, int tempStart,
+                                                           int minTargetFrontToCompare, int targetFrontUntil) {
+        for (int index = insertedFrontStart; index != -1; index = next[index], ++tempStart) {
+            ranks[index] = findRank(minTargetFrontToCompare, targetFrontUntil, index);
+            temp[tempStart] = index;
+        }
+        return tempStart;
+    }
+
+    private int writeOutAndFindRanksWithParentChecking(int insertedFrontStart, int tempStart, int targetFrontUntil) {
+        for (int index = insertedFrontStart; index != -1; index = next[index], ++tempStart) {
+            int myMinTargetFront = ranks[parents[index]] + 1;
+            ranks[index] = myMinTargetFront == targetFrontUntil
+                    ? myMinTargetFront
+                    : findRank(myMinTargetFront, targetFrontUntil, index);
+            temp[tempStart] = index;
+        }
+        return tempStart;
+    }
+
+    private int putToFronts(int pointIdx, int m, int targetFrontUntil) {
+        boolean frontMoved = false;
+        boolean allToMoved = true;
+        while (--pointIdx >= m) {
+            int index = temp[pointIdx];
+            int rankPtr = ranks[index];
+            int diff = targetFrontUntil - rankPtr;
+            if (diff == 0) {
+                frontMoved = true;
+                next[index] = -1;
+                ++targetFrontUntil;
+            } else {
+                allToMoved &= frontMoved && diff == 1;
+                next[index] = firstIndex[rankPtr];
+            }
+            firstIndex[rankPtr] = index;
+        }
+        return (frontMoved ? 1 : 0) ^ (allToMoved ? 2 : 0);
+    }
+
     private void merge(int l, int m, int n) {
         int r = Math.min(n, m + m - l);
-        int minTargetFrontToCompare = l - 1;
         int targetFrontUntil = l;
         while (targetFrontUntil < m && firstIndex[targetFrontUntil] != -1) {
             ++targetFrontUntil;
         }
 
-        for (int insertedFront = m; insertedFront < r; ++insertedFront) {
-            boolean isNotFirst = insertedFront != m;
-            int insertedFrontStart = firstIndex[insertedFront];
-            if (insertedFrontStart == -1) {
-                break;
-            }
-            if (++minTargetFrontToCompare == targetFrontUntil) {
-                if (insertedFront != targetFrontUntil) {
-                    firstIndex[targetFrontUntil] = insertedFrontStart;
-                    for (int i = insertedFrontStart; i != -1; i = next[i]) {
-                        ranks[i] = targetFrontUntil;
-                    }
-                }
-                ++targetFrontUntil;
-            } else {
-                // Find the ranks of the points from the current front, and write out the points in the `temp` array.
-                int pointIdx = m;
-                for (int index = insertedFrontStart; index != -1; index = next[index], ++pointIdx) {
-                    if (index < m || index >= r) {
-                        throw new AssertionError(m + " " + index + " " + r);
-                    }
-                    int myMinTargetFront = isNotFirst ? ranks[parents[index]] + 1 : minTargetFrontToCompare;
-                    ranks[index] = myMinTargetFront == targetFrontUntil
-                            ? myMinTargetFront
-                            : findRank(myMinTargetFront, targetFrontUntil, index);
-                    temp[pointIdx] = index;
-                }
-                // Integrate the solutions into the target fronts, starting from the last tested one.
-                minTargetFrontToCompare = targetFrontUntil;
-                while (--pointIdx >= m) {
-                    int index = temp[pointIdx];
-                    int rankPtr = ranks[index];
-                    if (targetFrontUntil == rankPtr) {
-                        ++targetFrontUntil;
-                        next[index] = -1;
-                    } else {
-                        if (minTargetFrontToCompare > rankPtr) {
-                            minTargetFrontToCompare = rankPtr;
-                        }
-                        next[index] = firstIndex[rankPtr];
-                    }
-                    firstIndex[rankPtr] = index;
+        // First front insertion is slightly special
+        int insertedFront = m, insertedFrontStart = firstIndex[insertedFront];
+        int firstPointIdx = writeOutAndFindRanksWithoutParentChecking(insertedFrontStart, m, l, targetFrontUntil);
+        int firstResult = putToFronts(firstPointIdx, m, targetFrontUntil);
+        targetFrontUntil += firstResult & 1;
+        ++insertedFront;
+
+        // General insertion if no preliminary break
+        if (firstResult <= 1) {
+            while (insertedFront < r && (insertedFrontStart = firstIndex[insertedFront]) != -1) {
+                int pointIdx = writeOutAndFindRanksWithParentChecking(insertedFrontStart, m, targetFrontUntil);
+                int result = putToFronts(pointIdx, m, targetFrontUntil);
+                targetFrontUntil += result & 1;
+                ++insertedFront;
+                if (result > 1) {
+                    break;
                 }
             }
         }
+
+        // Degenerate case if some fronts needs to be simply appended
+        if (insertedFront == targetFrontUntil) {
+            // Case 1: ranks need not be rewritten
+            while (insertedFront < r && firstIndex[insertedFront] != -1) {
+                ++insertedFront;
+                ++targetFrontUntil;
+            }
+        } else {
+            // Case 2: ranks need to be rewritten
+            while (insertedFront < r && (insertedFrontStart = firstIndex[insertedFront]) != -1) {
+                firstIndex[targetFrontUntil] = insertedFrontStart;
+                for (int i = insertedFrontStart; i != -1; i = next[i]) {
+                    ranks[i] = targetFrontUntil;
+                }
+                ++insertedFront;
+                ++targetFrontUntil;
+            }
+        }
+
+        // If there are fewer than N fronts, need to erase the next-to-last front pointer.
         if (targetFrontUntil < r) {
             firstIndex[targetFrontUntil] = -1;
         }
