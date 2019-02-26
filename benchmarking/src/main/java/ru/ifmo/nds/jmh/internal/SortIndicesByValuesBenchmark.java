3401e6bc34ced347e85a4b309f791a2d3ab09c86
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-02-26 20:39:52 +0300

    Update the benchmark parameters to reflect the region of interest.

--- benchmarking/src/main/java/ru/ifmo/nds/jmh/internal/SortIndicesByValuesBenchmark.java
+++ benchmarking/src/main/java/ru/ifmo/nds/jmh/internal/SortIndicesByValuesBenchmark.java
@@ -12,15 +12,14 @@ import ru.ifmo.nds.util.ArrayHelper;
 @BenchmarkMode(Mode.AverageTime)
 @OutputTimeUnit(TimeUnit.MICROSECONDS)
 @Timeout(time = 10)
-@Warmup(time = 1, iterations = 10)
-@Measurement(time = 1, iterations = 3)
-@Fork(value = 3)
+@Warmup(time = 1, iterations = 6)
+@Measurement(time = 1, iterations = 2)
+@Fork(value = 5)
 public class SortIndicesByValuesBenchmark {
-    @Param({"2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20",
-            "21", "22", "23", "24", "25", "30", "35", "40", "50", "60", "70", "80", "90", "100", "1000", "10000", "100000"})
+    @Param({"1", "2", "3", "4", "5", "7", "10", "13", "17", "23", "31", "42", "56", "74", "100", "133", "177", "237", "316"})
     private int size;
 
-    @Param({"10", "11", "12", "13", "14", "15", "16", "17", "18", "19", "20", "21", "22", "23", "24", "25"})
+    @Param({"10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32", "34", "36", "38", "40"})
     private int threshold;
 
     private int[][] data;
@@ -28,7 +27,7 @@ public class SortIndicesByValuesBenchmark {
 
     @Setup
     public void initialize() {
-        int nInstances = 10;
+        int nInstances = 100;
         Random random = new Random(size * 318325462111L);
         data = new int[nInstances][size];
         for (int i = 0; i < nInstances; ++i) {
@@ -39,6 +38,7 @@ public class SortIndicesByValuesBenchmark {
         indices = new int[size];
     }
 
+    @OperationsPerInvocation(100)
     @Benchmark
     public void run(Blackhole bh) {
         for (int[] instance : data) {
