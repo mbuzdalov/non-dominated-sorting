f5e421645714e967bf85258687452c39f4abd8d1
Maxim Buzdalov <mbuzdalov@gmail.com>
2019-03-01 01:22:23 +0300

    Revert the sort-indices-by-values benchmark to its initial shape.

--- benchmarking/src/main/java/ru/ifmo/nds/jmh/internal/SortIndicesByValuesBenchmark.java
+++ benchmarking/src/main/java/ru/ifmo/nds/jmh/internal/SortIndicesByValuesBenchmark.java
@@ -17,16 +17,9 @@ import ru.ifmo.nds.util.ArraySorter;
 @Measurement(time = 1, iterations = 2)
 @Fork(value = 5)
 public class SortIndicesByValuesBenchmark {
-    @Param({"1", "2", "3", "4", "5", "7", "10", "13", "17", "23", "31", "42", "56", "74", "100", "133", "177", "237", "316"})
+    @Param({"1", "2", "3", "4", "5", "7", "10", "13", "17", "23",
+            "31", "42", "56", "74", "100", "133", "177", "237", "316"})
     private int size;
-
-    @Param({"1",
-            "10", "12", "14", "16", "18", "20", "22", "24", "26", "28", "30", "32", "34", "36", "38",
-            "40", "42", "44", "46", "48", "50", "52", "54", "56", "58", "60", "62", "64", "66", "68",
-            "70", "72", "74", "76", "78", "80", "82", "84", "86", "88", "90", "92", "94", "96", "98",
-            "100", "102", "104", "108", "112", "116", "120", "124", "128", "132", "136", "140", "144",
-            "148", "152", "156", "160", "164", "168", "172", "176", "180", "184", "188", "196", "200"})
-    private int threshold;
 
     private int[][] data;
     private int[] indices;
@@ -47,63 +40,9 @@ public class SortIndicesByValuesBenchmark {
     @OperationsPerInvocation(100)
     @Benchmark
     public void run(Blackhole bh) {
-        if (threshold == 1) {
-            for (int[] instance : data) {
-                ArrayHelper.fillIdentity(indices, size);
-                ArraySorter.sortIndicesByValues(indices, instance, 0, size);
-            }
-        } else {
-            for (int[] instance : data) {
-                ArrayHelper.fillIdentity(indices, size);
-                sortIndicesByValues(indices, instance, 0, size);
-            }
-        }
-    }
-
-    private static long splitIndicesByRanks(int[] indices, int[] values, int from, int until) {
-        int left = from, right = until - 1;
-        int pivot = values[indices[(from + until) >>> 1]];
-        int sl, sr;
-        while (left <= right) {
-            while (values[sl = indices[left]] < pivot) ++left;
-            while (values[sr = indices[right]] > pivot) --right;
-            if (left <= right) {
-                indices[left] = sr;
-                indices[right] = sl;
-                ++left;
-                --right;
-            }
-        }
-        return ((long) (right) << 32) ^ left; // left is non-negative
-    }
-
-    private static void insertionSortIndicesByValues(int[] indices, int[] values, int from, int to) {
-        for (int i = from, j = i; i < to; j = i) {
-            int ii = indices[++i], ij;
-            int ai = values[ii];
-            while (ai < values[ij = indices[j]]) {
-                indices[j + 1] = ij;
-                if (--j < from) {
-                    break;
-                }
-            }
-            indices[j + 1] = ii;
-        }
-    }
-
-    private void sortIndicesByValues(int[] indices, int[] values, int from, int until) {
-        if (from + threshold > until) {
-            insertionSortIndicesByValues(indices, values, from, until - 1);
-        } else {
-            long pack = splitIndicesByRanks(indices, values, from, until);
-            int left = (int) pack;
-            int right = (int) (pack >> 32);
-            if (from < right) {
-                sortIndicesByValues(indices, values, from, right + 1);
-            }
-            if (left + 1 < until) {
-                sortIndicesByValues(indices, values, left, until);
-            }
+        for (int[] instance : data) {
+            ArrayHelper.fillIdentity(indices, size);
+            ArraySorter.sortIndicesByValues(indices, instance, 0, size);
         }
     }
 }
